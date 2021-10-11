27172efbf717c6cc53031f6e4a4e2f633e4f89e9
evgen-jkay <eviljkay@gmail.com>
2021-10-11 19:55:27 +0300

    add ubuntu

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -3,8 +3,9 @@ package com.metanit;
 
 public class Main {
 
-  public static void main(String[] args) { 
-      final String javaStr = "Java";
-      System.out.println("Hello " + javaStr);
+  public static void main(String[] args) {
+      for (int i = 1; i < 9; i++){
+          System.out.printf("Квадрат числа %d равен %d \n", i, i * i);
+      }
   };
 }
