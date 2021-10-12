7dfdc7568749dcc57122402500f8da49056398dc
Evgen <evgenlandarenko@gmail.com>
2021-10-12 21:58:59 +0300

    Update Main.java

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,10 +1,12 @@
 package com.main;
 
 import com.myApp.Salary;
+import com.book.Book;
 
 
 public class Main {
     public static void main(String[] args) {
         Salary.mySalary();
+        Book.book();
     }
 }
