869bfea029d79cdfb9138414497782cb8622e1df
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-14 18:40:22 +0300

    Convert Arrays To String

---
+++ src/main/java/com/example/ConvertArraysToString.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class ConvertArraysToString {
+    public static void main(String[] args) {
+        int[] arrays = {222, 333, 444};
+
+        System.out.println(Arrays.toString(arrays));
+    }
+}
