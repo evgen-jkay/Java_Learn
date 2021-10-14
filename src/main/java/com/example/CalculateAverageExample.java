f3489522ecffe23e36cd218fa7743729ce092f36
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-14 19:17:45 +0300

    Calculate Average Example

---
+++ src/main/java/com/example/CalculateAverageExample.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class CalculateAverageExample {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {22, 20, 41, 55, 16, 60, 100};
+
+        int sum = 0;
+
+        /* Вариант 1: */
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+
+        /*
+         * Вариант 2:
+         *
+         * for (int i = 0; i < numbers.length; i++) {
+         *    sum2 += numbers[i];
+         * }
+         */
+
+        /*
+         * Вариант 3:
+         *
+         * for (int number : numbers) sum3 += number;
+         */
+
+        System.out.println(sum);
+
+        float average = sum / numbers.length;
+
+        System.out.println("Average is: " + average);
+    }
+}
