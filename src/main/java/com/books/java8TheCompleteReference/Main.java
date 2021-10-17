bb653dab2e27fe5a7f87f798fb7924e458f9de18
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-17 16:24:55 +0300

    book

--- src/main/java/com/books/java8TheCompleteReference/Main.java
+++ src/main/java/com/books/java8TheCompleteReference/Main.java
@@ -2,6 +2,6 @@ package com.books.java8TheCompleteReference;
 
 public class Main {
     public static void main(String[] args) { 
-        System.out.println("Hello Java");
+        System.out.println("Java 8. Полное руководство");
     }
 }
