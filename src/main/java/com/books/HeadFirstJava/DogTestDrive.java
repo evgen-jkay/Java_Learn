d7a4fed910423b44f75b0495bd591140569f444e
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-20 14:37:25 +0300

    example incaps

--- src/main/java/com/books/HeadFirstJava/DogTestDrive.java
+++ src/main/java/com/books/HeadFirstJava/DogTestDrive.java
@@ -4,10 +4,14 @@ public class DogTestDrive {
 
     public static void main(String[] args) {
         Dog one = new Dog();
+        Dog two = new Dog();
         one.setSize(70);
+        two.setSize(8);
 
         System.out.println("First dog: " + one.getSize());
+        System.out.println("Second dog: " + two.getSize());
         one.bark();
+        two.bark();
     }
 
 }
