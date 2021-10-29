7510677513117d8d690769d6c4941536c153f1b4
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-29 09:39:20 +0300

    Упраднение главы 1

---
+++ src/main/java/com/books/HeadFirstJava/exercises/сhapter1/ExerciselbA.java
@@ -0,0 +1,13 @@
+package com.books.HeadFirstJava.exercises.сhapter1;
+
+public class ExerciselbA {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            x++;
+            if (x > 3) {
+                System.out.println("большой икс");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/books/HeadFirstJava/exercises/сhapter1/ExerciselbC.java
@@ -0,0 +1,13 @@
+package com.books.HeadFirstJava.exercises.сhapter1;
+
+public class ExerciselbC {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 1) {
+            x--;
+            if (x < 3) {
+                System.out.println("маленький вес");
+            }
+        }
+    }
+}
