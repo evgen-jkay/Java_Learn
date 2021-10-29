a10a2959fabeb9d9f9398cd3d810c13f6ac9a992
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-29 10:16:14 +0300

    Упраднение главы 2

---
+++ src/main/java/com/books/HeadFirstJava/exercises/сhapter2/DVDPlayer.java
@@ -0,0 +1,27 @@
+package com.books.HeadFirstJava.exercises.сhapter2;
+
+class DVDPlayer {
+
+    boolean cenRecord = false;
+
+    void recordDVD() {
+        System.out.println("идет запись DVD");
+    }
+
+    void prayDVD() {
+        System.out.println("DVD протгрывается");
+    }
+
+}
+
+class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.cenRecord = true;
+        d.prayDVD();
+
+        if (d.cenRecord = true) {
+            d.recordDVD();
+        }
+    }
+}
---
+++ src/main/java/com/books/HeadFirstJava/exercises/сhapter2/DrumKit.java
@@ -0,0 +1,29 @@
+package com.books.HeadFirstJava.exercises.сhapter2;
+
+class DrumKit {
+
+    boolean topHat = true;
+    boolean share = true;
+
+    void playTopHat() {
+        System.out.println("динь динь ди-динь");
+    }
+
+    void playShare() {
+        System.out.println("бах бах ба-бах");
+    }
+}
+
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+
+        DrumKit d = new DrumKit();
+        d.playShare();
+        d.share = false;
+        d.playTopHat();
+
+        if (d.share == true) {
+            d.playShare();
+        }
+    }
+}
