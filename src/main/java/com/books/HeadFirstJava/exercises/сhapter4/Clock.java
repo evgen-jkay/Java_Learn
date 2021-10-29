d1b3542aefc23f2329109136d173b1b3ed696c56
evgen-jkay <evgenlandarenko@gmail.com>
2021-10-29 10:54:20 +0300

    Упраднение главы 4

---
+++ src/main/java/com/books/HeadFirstJava/exercises/сhapter4/Clock.java
@@ -0,0 +1,23 @@
+package com.books.HeadFirstJava.exercises.сhapter4;
+
+class Clock {
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+
+    String getTime() {
+        return time;
+    }
+}
+
+class ClockTestDrive {
+    public static void main(String[] args) {
+        Clock c = new Clock();
+
+        c.setTime("12345");
+        String tod = c.getTime();
+        System.out.println("время " + tod);
+    }
+}
