335dbc5870ef9a790eb7a601d61fce9d78089bfe
evgen-jkay <evgenlandarenko@gmail.com>
2021-12-03 09:38:42 +0200

    Array

---
+++ src/main/java/com/books/OOPinJava/exercises/сhapter01/TestAuthor.java
@@ -0,0 +1,8 @@
+package com.books.OOPinJava.exercises.сhapter01;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author Ivanov = new Author("Ivanov", "ivan@sobaka.loh");
+        System.out.println(Ivanov);
+    }
+}
---
+++ src/main/java/com/books/OOPinJava/exercises/сhapter01/TestBook.java
@@ -0,0 +1,7 @@
+package com.books.OOPinJava.exercises.сhapter01;
+
+public class TestBook {
+    public static void main(String[] args) {
+        Author Ivanov = new Author("Ivan Ivanov", "ivanov@kuda.com");
+    }
+}
