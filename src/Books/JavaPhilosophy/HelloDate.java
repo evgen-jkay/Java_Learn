0c86d1ca1e309e1e6262dbd8219dbcd710fc2ef0
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-26 21:45:45 +0300

    ...

---
+++ src/Books/JavaPhilosophy/HelloDate.java
@@ -0,0 +1,19 @@
+package Books.JavaPhilosophy;
+
+import java.util.Date;
+
+/**
+ * Виводе строку та нинішню дату
+ * @author Євген Ландаренко
+ * @version 4.0
+ */
+public class HelloDate {
+    /**
+     * @param args массив строкових аргументів
+     * @throws exceptions виключень не видає
+     */
+    public static void main(String[] args) {
+        System.out.println("Привіт, сьогодні: ");
+        System.out.println(new Date());
+    }
+}
