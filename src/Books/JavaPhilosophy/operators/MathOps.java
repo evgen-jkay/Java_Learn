7e96a4f10376de399203ae015badbf8fe31f636e
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-29 23:25:00 +0300

    operators

---
+++ src/Books/JavaPhilosophy/operators/MathOps.java
@@ -0,0 +1,26 @@
+package Books.JavaPhilosophy.operators;
+
+import java.util.Random;
+
+public class MathOps {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        int i, j, k;
+        j = rand.nextInt(100) + 1;
+        System.out.println("j : " + j);
+        k = rand.nextInt(100) + 1;
+        System.out.println("k : " + k);
+        i = j + k;
+        System.out.println(" j + k : " + i);
+        i = j - k;
+        System.out.println("j - k : " + i);
+        i = k / j;
+        System.out.println("k / j : " + i);
+        i = k * j;
+        System.out.println("k * j : " + i);
+        i = k % j;
+        System.out.println("k % j : " + i);
+        j %= k;
+        System.out.println("j %/ k : " + i);
+    }
+}
