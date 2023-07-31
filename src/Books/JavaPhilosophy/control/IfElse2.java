a33faa385025b520270dd3ee464d4663a60c843d
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-31 23:03:38 +0300

    control

---
+++ src/Books/JavaPhilosophy/control/IfElse2.java
@@ -0,0 +1,18 @@
+package Books.JavaPhilosophy.control;
+
+public class IfElse2 {
+    static int test(int testVal, int target) {
+        if (testVal > target)
+            return +1;
+        else if (testVal < target)
+            return -1;
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test(10, 5));
+        System.out.println(test(5, 10));
+        System.out.println(test(5, 5));
+    }
+}
