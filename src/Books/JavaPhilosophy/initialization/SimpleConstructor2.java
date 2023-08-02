757397f9f5893b57d9a00993c0d85d93121b4c02
evgen-jkay <evgenlandarenko@gmail.com>
2023-08-02 10:37:01 +0300

    initialization

---
+++ src/Books/JavaPhilosophy/initialization/SimpleConstructor2.java
@@ -0,0 +1,15 @@
+package Books.JavaPhilosophy.initialization;
+
+public class SimpleConstructor2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            new Rock2(i);
+        }
+    }
+}
+
+class Rock2 {
+    Rock2(int i) {
+        System.out.print("Rock " + i + " ");
+    }
+}
